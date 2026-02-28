e4fa3df3cc9ec8443c2f8efecbb5072a91b15f3f
Raghusai Achuth <raghusai.achuth@mphasis.com>
2026-02-28 16:05:21 +0530

    hibernatestudent

---
+++ SpringBootRestAPIDB/src/main/java/com/test/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.test.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+@Schema
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	public Customer() {
+		
+	}
+
+	public Customer(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
