26a379922d5890a939b99a260b55ec06f3105966
Raghusai Achuth <raghusai.achuth@mphasis.com>
2026-03-04 10:06:47 +0530

    SpringSecurity

---
+++ SpringBootSecurityJPA/src/main/java/com/test/repository/RolesRepository.java
@@ -0,0 +1,10 @@
+package com.test.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.test.entity.Role;
+
+public interface RolesRepository extends JpaRepository<Role, Integer>
+{
+
+}
